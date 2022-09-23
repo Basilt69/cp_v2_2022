2b20e7bc2d79ed75488e818d08f482f448038125
basiltkachenko <baziltkachenko@gmail.com>
2022-09-23 13:45:30 +0300

    first step

--- server.c
+++ server.c
@@ -20,4 +20,31 @@ void cleanup() {
     close(sFd);
 }
 
-void
+void sighandler(int sig) {
+    cleanup();
+    printf("\nЗавершение работы...\n");
+    exit(0);
+}
+
+int main() {
+    sFd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sFd == -1) {
+        printf("Socket error\n");
+        return -1;
+    }
+    struct epoll_event events[MAX_EPOLL_EVENTS];
+    int epfd = epoll_create(1);
+    struct epoll_event event;
+    event.events = EPOLLIN | EPOLLOUT;
+    event.data.fd = sFd;
+    epoll_ctl(epfd, EPOLL_CTL_ADD, sFd, &event);
+    struct sockaddr_in servaddr;
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+    servaddr.sin_port = htons(PORT);
+    if (bind(sFd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0) {
+        printf("Bind error\n");
+        cleanup();
+        return -1;
+    }
+}
